ee163c851bb98a083a68678367751da81ef723fb
William A <wandreopoulos@lbl.gov>
2024-08-28 19:52:13 +0000

    this is q0

--- C_intro_examples/0b.c
+++ C_intro_examples/0b.c
@@ -3,14 +3,26 @@ int main()
 {
  int i = 0; 
  while(i < 5) {
-  printf("%d", i);
+  printf("%d\n", i);
   ++i;
  } 
 
   i = 0;
  while(i < 5) {
-  printf("%d", i);
+  printf("%d\n", i);
   i++;
  }
+
+ int array1D[4] = {1,2,3,4};
+int array1Db[] = {1,2,3,4};
+
+int itemsList[] = {1,2,3,4};
+
+for (i = 0; i < 4; ++i) {
+   itemsList[i] = itemsList[i+1];
+   printf("%d\n", itemsList[i]);
+}
+
+
 }
 
