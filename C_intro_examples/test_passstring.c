60aa68b5675f01a48924a089000704f963c331f1
William A <wandreopoulos@lbl.gov>
2025-10-16 02:35:40 +0000

    one file

---
+++ C_intro_examples/test_passstring.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+//char * getString(char str[]) // *str) //it is not necessary to return anything!
+//if you pass in dynamic malloc'ed memory the modifications to mem in the function 
+//will persist after the func returns.
+    //return (char *)str;
+
+void getString(char str[]) // char *str) 
+// you can pass either a [] or a pointer * and it wil be treated like a pointer by C/C++
+{
+    str[0] = 'w';
+}
+
+int main()
+{
+    char * s = calloc(10, 1);  //malloc and calloc is how you declare dynamic memory
+    //getString("Will I be printed?"); //this will not work because what you passing
+    //      in is a string literal on the Data Segment, not dynamic memory on the heap
+    getString(s);
+    printf("%s", s);
+    //getchar();
+    return 0;
+}
+
+
